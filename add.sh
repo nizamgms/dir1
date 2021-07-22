31ee69535b0f998f862326b593bce4429e3996ea
nizamgms <mdnizam.gms@gmail.com>
2021-07-22 12:17:57 +0000

    commited 2 files

---
+++ add.sh
@@ -0,0 +1,6 @@
+#!/bin/bash.
+echo -n "Enter the two numbers : "
+read num1 num2
+sum=`expr $num1 + $num2`
+echo "sum of two numbers is $sum"
+
---
+++ add2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash.
+echo -n "enter the two numbers : "
+read num1 num2
+sum="expr $num1 + $num2"
+echo "the sum is $sum"
